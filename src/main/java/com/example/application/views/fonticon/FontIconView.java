2ee855b6a851d013d053ab0c11bf5833d9180b6c
Tomi Virkki <virkki@vaadin.com>
2023-08-31 10:44:44 +0300

    solution

--- src/main/java/com/example/application/views/fonticon/FontIconView.java
+++ src/main/java/com/example/application/views/fonticon/FontIconView.java
@@ -1,6 +1,8 @@
 package com.example.application.views.fonticon;
 
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.FontIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -12,6 +14,32 @@ import com.vaadin.flow.router.RouteAlias;
 public class FontIconView extends VerticalLayout {
 
     public FontIconView() {
-        
+        var icon = LumoIcons.BELL.create();
+        var button = new Button("Change icon", e -> {
+            icon.setCharCode(LumoIcons.COG.getCharCode());
+        });
+
+        add(icon, button);
+    }
+
+    enum LumoIcons {
+        BELL("\uea0d"), COG("\uea15");
+
+        private String codepoint;
+
+        LumoIcons(String codepoint) {
+            this.codepoint = codepoint;
+        }
+
+        public FontIcon create() {
+            var icon = new FontIcon();
+            icon.setFontFamily("Lumo icons");
+            icon.setCharCode(codepoint);
+            return icon;
+        }
+
+        public String getCharCode() {
+            return codepoint;
+        }
     }
 }
