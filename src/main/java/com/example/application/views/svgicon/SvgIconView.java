da9d0ca06c2a3a5a4c2f811394805cfd8c04c84f
Diego Cardoso <diego@vaadin.com>
2023-08-31 11:57:24 +0300

    solution

--- src/main/java/com/example/application/views/svgicon/SvgIconView.java
+++ src/main/java/com/example/application/views/svgicon/SvgIconView.java
@@ -1,6 +1,8 @@
 package com.example.application.views.svgicon;
 
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.SvgIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -10,7 +12,12 @@ import com.vaadin.flow.router.Route;
 public class SvgIconView extends VerticalLayout {
 
     public SvgIconView() {
+        var icon = new SvgIcon("/themes/myapp/code-branch.svg");
 
+        var button = new Button("Change color", e -> {
+            icon.setColor("#1a81fa");
+        });
+
+        add(icon, button);
     }
-
 }
