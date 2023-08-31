495d26e8b2b8d4e530d233292e32d62723272ecd
Tomi Virkki <virkki@vaadin.com>
2023-08-31 09:51:48 +0300

    solution

--- src/main/java/com/example/application/views/fonticon/FontIconView.java
+++ src/main/java/com/example/application/views/fonticon/FontIconView.java
@@ -1,6 +1,8 @@
 package com.example.application.views.fonticon;
 
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.FontIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -12,6 +14,31 @@ import com.vaadin.flow.router.RouteAlias;
 public class FontIconView extends VerticalLayout {
 
     public FontIconView() {
-        
+
+        var icon = FontAwesomeIcons.USER.create();
+        var button = new Button("Change icon", e -> {
+            icon.setFont(FontAwesomeIcons.CAMERA.getFont());
+        });
+
+        add(icon, button);
     }
+
+        enum FontAwesomeIcons {
+            CAMERA("fa-camera"), USER("fa-user");
+
+            private String iconClassName;
+
+            FontAwesomeIcons(String iconClassName) {
+                this.iconClassName = iconClassName;
+            }
+
+            public FontIcon create() {
+                return new FontIcon(getFont());
+            }
+
+            public String[] getFont() {
+                return new String[] { "fa-solid", iconClassName };
+            }
+        }
+
 }
