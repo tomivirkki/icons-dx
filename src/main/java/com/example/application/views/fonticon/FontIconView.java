e4fd7e6f384d3efeeb95f6a21dfea849d8be1872
Tomi Virkki <virkki@vaadin.com>
2023-08-31 11:22:29 +0300

    solution

--- src/main/java/com/example/application/views/fonticon/FontIconView.java
+++ src/main/java/com/example/application/views/fonticon/FontIconView.java
@@ -1,6 +1,8 @@
 package com.example.application.views.fonticon;
 
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.FontIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -12,6 +14,33 @@ import com.vaadin.flow.router.RouteAlias;
 public class FontIconView extends VerticalLayout {
 
     public FontIconView() {
-        
+        var icon = MaterialIcons.FACE.create();
+        var button = new Button("Change icon", e -> {
+            icon.setCharCode(MaterialIcons.LIGHTBULB.getCharCode());
+        });
+
+        add(icon, button);
     }
+
+    enum MaterialIcons {
+        FACE("face"), LIGHTBULB("lightbulb");
+
+        private String ligature;
+
+        MaterialIcons(String ligature) {
+            this.ligature = ligature;
+        }
+
+        public FontIcon create() {
+            var icon = new FontIcon("material-icons");
+            icon.setCharCode(getCharCode());
+            return icon;
+        }
+
+        public String getCharCode() {
+            return ligature;
+        }
+    }
+
+
 }
