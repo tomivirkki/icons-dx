c7e78ad6d99702d981e65c248a4657af3203bd95
Diego Cardoso <diego@vaadin.com>
2023-08-31 12:55:08 +0300

    solution

--- src/main/java/com/example/application/views/svgicon/SvgIconView.java
+++ src/main/java/com/example/application/views/svgicon/SvgIconView.java
@@ -1,6 +1,7 @@
 package com.example.application.views.svgicon;
 
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.icon.SvgIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -10,6 +11,17 @@ import com.vaadin.flow.router.Route;
 public class SvgIconView extends VerticalLayout {
 
     public SvgIconView() {
+        add(FAIcons.CODE_BRANCH.create(), FAIcons.USER.create());
+    }
+
+    enum FAIcons {
+        USER, CODE_BRANCH;
+
+        public SvgIcon create() {
+            var src = String.format("/themes/myapp/solid.svg#%s",
+                    this.name().toLowerCase().replace("_", "-"));
+            return new SvgIcon(src);
+        }
 
     }
 
